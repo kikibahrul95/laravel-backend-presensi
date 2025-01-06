0d021e3b6a4ac63c9ef1ef68a3a5887234b873de
kikibahrul95 <kikibahrul@gmail.com>
2025-01-06 13:05:37 +0700

    office radius

--- app/Models/Schedule.php
+++ app/Models/Schedule.php
@@ -4,8 +4,31 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Schedule extends Model
 {
+
+    protected $fillable = [
+        'user_id',
+        'shift_id',
+        'office_id',
+        
+
+    ];
     use HasFactory;
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(user::class);
+    }
+
+    public function shift(): BelongsTo
+    {
+        return $this->belongsTo(Shift::class);
+    }
+    public function office(): BelongsTo
+    {
+        return $this->belongsTo(Office::class);
+    }
 }
