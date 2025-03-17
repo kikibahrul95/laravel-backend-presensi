aee142d935b83264774c82ce0504383e5e11cdee
kikibahrul95 <kikibahrul@gmail.com>
2025-03-17 11:26:55 +0700

    tampilan peta

--- resources/views/livewire/presensi.blade.php
+++ resources/views/livewire/presensi.blade.php
@@ -1,3 +1,20 @@
 <div>
-    <h1>Tempat absen</h1>
+    <div class="container mx-auto">
+        <div class="bg-white p-6 rounded-lg mt-3  shadow-lg" >
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-6 mb-6">
+  <div>
+    <h2 class="text-2xl font-bold mb-2">Informasi Pegawai</h2>
+    <div class="bg-gray-100 p-4 rounded-lg">
+        <p><strong>Nama Pegawai :</strong> {{Auth::user()->name}}</p>
+        <p><strong>Kantor :</strong>kantor utama</p>
+        <p><strong>Shift :</strong>pagi</p>
+  </div>
+  <div>
+
+
+
+</div>
+
+</div>
+    </div>
 </div>
